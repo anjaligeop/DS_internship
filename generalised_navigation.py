bb7f07a9510a9b5aef2c9e07d70a22e9a3a2d986
anjaligeop <anjaligeorgep@gmail.com>
2020-03-17 08:55:18 +0530

    generalised login-nav-logout , implementation with class

--- generalised_navigation.py
+++ generalised_navigation.py
@@ -1,103 +1,121 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
-import os
 import time
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 import time
-import driverfile as dfr
-import config as cfg
+import driverfile as dfr    #configuration file
+import config as cfg        #configuration file
 
-driver_path = cfg.webdriver_path["chrome"]
-driver=eval(driver_path)
 
-def login(bankname):
-    driver.get(cfg.bank_url[bankname])
-    if bankname.upper() == "CITI":
-        eval(dfr.findElement(cfg.login_id[bankname]["loginbtn"])).click()
-        child = driver.window_handles[1]
-        driver.switch_to.window(child)
-    eval(dfr.findElement(cfg.login_id[bankname]["userid"])).click()
-    eval(dfr.findElement(cfg.login_id[bankname]["userid"])).send_keys('username')
-    if bankname.upper() == "CITI":
-        eval(dfr.findElement(cfg.login_id[bankname]["keyboardid"])).click()
-    eval(dfr.findElement(cfg.login_id[bankname]["pwid"])).click()
-    eval(dfr.findElement(cfg.login_id[bankname]["pwid"])).send_keys('pwd')
-    if bankname.upper()=="CANARA":
-        eval(dfr.findElement(cfg.login_id[bankname]["captchaid"])).click()
-        a = input("captcha?")
-        eval(dfr.findElement(cfg.login_id[bankname]["captchaid"])).send_keys(a)
-    eval(dfr.findElement(cfg.login_id[bankname]["clicklogin"])).click()
-    if bankname.upper()=="CANARA":
-        child = driver.window_handles[1]
-        driver.switch_to.window(child)
-def logout(bankname):
-    if bankname.upper()=="FEDERAL":
-        eval(dfr.findElement(cfg.logout_id[bankname]["logoutnav"])).click()
-        eval(dfr.findElement(cfg.logout_id[bankname]["logoutbtn"])).click()
-        driver.close()
-    elif bankname.upper()=="CITI":
-        driver.switch_to.frame(0)
-        eval(dfr.findElement(cfg.logout_id[bankname]["logoutbtn"])).click()
-        driver.close()
-    elif bankname.upper()=="CANARA":
-        time.sleep(5)
-        driver.switch_to.frame(0)
-        eval(dfr.findElement(cfg.logout_id[bankname]["logoutbtn"])).click()
-        driver.switch_to.default_content()
-        eval(dfr.findElement(cfg.logout_id[bankname]["closewindow"])).click()
-
-def download_html(bankname):
-    a=driver.page_source
+def download_html(bankname,driver):
+    a = driver.page_source
     file_nm = open('/home/anjaligeorgep/Desktop/download_html/file_{}.html'.format(bankname), 'w')
     file_nm.write(a)
 
-def navigate_transaction(bankname):
-    if bankname.upper()=="FEDERAL":
-        eval(dfr.findElement(cfg.navigate_id[bankname]["click0"])).click()
-        eval(dfr.findElement(cfg.navigate_id[bankname]["click1"])).click()
-        eval(dfr.findElement(cfg.navigate_id[bankname]["click2"])).click()
-        eval(dfr.findElement(cfg.navigate_id[bankname]["click3"])).click()
-        #click on download in pdf format
-        download_html("federal")
-    elif bankname.upper()=="CITI":
-        time.sleep(3)
-        driver.switch_to.frame(2)
-        eval(dfr.findElement(cfg.navigate_id[bankname]["click0"])).click()
-        driver.switch_to.default_content()
-        time.sleep(3)
-        driver.switch_to.frame(3)
-        eval(dfr.findElement(cfg.navigate_id[bankname]["click1"])).click()
-        eval(dfr.findElement(cfg.navigate_id[bankname]["click2"])).click()
-        download_html("citi")
-        driver.switch_to.default_content()
-        #click on download in pdf format
 
-    elif bankname.upper()=="CANARA":
-        time.sleep(5)
-        driver.switch_to.frame(1)
-        eval(dfr.findElement(cfg.navigate_id[bankname]["click0"])).click()
-        eval(dfr.findElement(cfg.navigate_id[bankname]["click1"])).click()
-        driver.switch_to.default_content()
-        time.sleep(5)
-        driver.switch_to.frame(4)
-        eval(dfr.findElement(cfg.navigate_id[bankname]["click2"])).click()
-        eval(dfr.findElement(cfg.navigate_id[bankname]["click3"])).click()  #account detail(wrong xpath generated")
-        download_html("canara")
-        driver.switch_to.default_content()
-        #click on download in req format
+class Driver_ini:
+    def get_driver(self):
+        driver_path = cfg.webdriver_path["chrome"]
+        self.driver = eval(driver_path)
+        return self.driver
+
+
+
+class Navigate():
+    def __init__(self,bankname):
+        self.bankname = bankname
+
+    def login(self,bankname,driver):
+        driver.get(cfg.bank_url[bankname])
+        if bankname.upper() == "CITI":
+            eval(dfr.findElement(cfg.login_id[bankname]["loginbtn"])).click()
+            child = driver.window_handles[1]
+            driver.switch_to.window(child)
+        eval(dfr.findElement(cfg.login_id[bankname]["userid"])).click()
+        eval(dfr.findElement(cfg.login_id[bankname]["userid"])).send_keys('username') #user name to be entered
+        if bankname.upper() == "CITI":
+            eval(dfr.findElement(cfg.login_id[bankname]["keyboardid"])).click()
+        eval(dfr.findElement(cfg.login_id[bankname]["pwid"])).click()
+        eval(dfr.findElement(cfg.login_id[bankname]["pwid"])).send_keys("pwd") #password to be entered
+        if bankname.upper()=="CANARA":
+            eval(dfr.findElement(cfg.login_id[bankname]["captchaid"])).click()
+            a = input("captcha?")
+            eval(dfr.findElement(cfg.login_id[bankname]["captchaid"])).send_keys(a)
+        eval(dfr.findElement(cfg.login_id[bankname]["clicklogin"])).click()
+        if bankname.upper()=="CANARA":
+            child = driver.window_handles[1]
+            driver.switch_to.window(child)
+
+    def logout(self,bankname,driver):
+        if bankname.upper()=="FEDERAL":
+            eval(dfr.findElement(cfg.logout_id[bankname]["logoutnav"])).click()
+            eval(dfr.findElement(cfg.logout_id[bankname]["logoutbtn"])).click()
+            driver.close()
+        elif bankname.upper()=="CITI":
+            driver.switch_to.frame(0)
+            eval(dfr.findElement(cfg.logout_id[bankname]["logoutbtn"])).click()
+            driver.close()
+        elif bankname.upper()=="CANARA":
+            time.sleep(5)
+            driver.switch_to.frame(0)
+            eval(dfr.findElement(cfg.logout_id[bankname]["logoutbtn"])).click()
+            driver.switch_to.default_content()
+            driver.close()
+
+    def navigate_transaction(self,bankname,driver):
+        if bankname.upper()=="FEDERAL":
+            eval(dfr.findElement(cfg.navigate_id[bankname]["click0"])).click()
+            eval(dfr.findElement(cfg.navigate_id[bankname]["click1"])).click()
+            eval(dfr.findElement(cfg.navigate_id[bankname]["click2"])).click()
+            eval(dfr.findElement(cfg.navigate_id[bankname]["click3"])).click()
+            #click on download in pdf format
+            download_html("federal",driver)
+
+        elif bankname.upper()=="CITI":
+            time.sleep(3)
+            driver.switch_to.frame(2)
+            eval(dfr.findElement(cfg.navigate_id[bankname]["click0"])).click()
+            driver.switch_to.default_content()
+            time.sleep(3)
+            driver.switch_to.frame(3)
+            eval(dfr.findElement(cfg.navigate_id[bankname]["click1"])).click()
+            eval(dfr.findElement(cfg.navigate_id[bankname]["click2"])).click()
+            download_html("citi",driver)
+            driver.switch_to.default_content()
+            #click on download in pdf format
+
+        elif bankname.upper()=="CANARA":
+            time.sleep(5)
+            driver.switch_to.frame(1)
+            eval(dfr.findElement(cfg.navigate_id[bankname]["click0"])).click()
+            eval(dfr.findElement(cfg.navigate_id[bankname]["click1"])).click()
+            driver.switch_to.default_content()
+            time.sleep(5)
+            driver.switch_to.frame(4)
+            eval(dfr.findElement(cfg.navigate_id[bankname]["click2"])).click()
+            eval(dfr.findElement(cfg.navigate_id[bankname]["click3"])).click()  #account detail(wrong xpath generated")
+            download_html("canara",driver)
+            driver.switch_to.default_content()
+            #click on download in req format
 
 if __name__=="__main__":
     try:
-        bnm="federal"
-        login(bnm)
-        navigate_transaction(bnm)
-        logout(bnm)
+        bnm = "federal"     #input("enter bankname")
+        dr = Driver_ini()
+        driver = dr.get_driver()
+
+        n = Navigate(bnm)
+        n.login(bnm, driver)
+        n.navigate_transaction(bnm, driver)
+        n.logout(bnm, driver)
+
+
+
+
 
     except Exception as e:
         print("**ERROR**")
         print(e.__doc__)
-    
 
 
 
